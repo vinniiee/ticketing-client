db9f0a45efb0eec9903ac2c12348ec2da23b727a
vinniiee <vs28245@gmail.com>
2022-09-23 02:06:54 +0530

    complete header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+module.exports = {
 
-module.exports = nextConfig
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: { plugins: [
+              {
+                name: 'preset-default',
+                params: {
+                  overrides: { removeViewBox: false },
+                },
+              },
+            ], },
+            titleProp: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from "./scss/index.module.scss";
+import Image from "next/future/image";
+import logo from "../../public/img/black-logo.png";
+import SearchIcon from "../../public/icons/SVG/search.svg";
+import BookmarkIcon from "../../public/icons/SVG/bookmark.svg";
+import userIcon from "../../public/user/default-user-image.jpg";
+
+
 
 function AllEvents() {
+  
   return (
     <div className={styles.container}>
-      <header className={styles.header}>header</header>
+      <header className={styles.header}>
+        <Image  src={logo} className={styles.headerLogo}  alt="brandLogo" ></Image>
+        <form className={styles.searchBar} >
+          <input className={styles.searchBarInput}  placeholder="Search Events"/>
+          <button className={styles.searchBarButton}>
+          {/* <svg src={searchIcon} className={styles.searchBarIcon} >
+            <use xlink:href='icons/sprite.svg#search'></use>
+          </svg> */}
+          <SearchIcon className={styles.searchBarIcon}  />
+          </button>
+        </form>
+        <div className={styles.userNav}>
+          <button className={styles.userNavBookmarkButton}>
+            <BookmarkIcon className={styles.userNavBookmarkIcon} ></BookmarkIcon>
+            <span className={styles.userNavBookmarkIconNotification}>99+</span>
+          </button>
+          <button className={styles.userNavProfileButton}>
+            <Image src={userIcon} className={styles.userNavProfileIcon} alt="user-icon"></Image>
+            <span className={styles.userNavUsername}>FIRST_NAME</span>
+          </button>
+
+        </div>
+
+      </header>
       <div className={styles.content}>
         <nav className={styles.sideBar}>Side Bar</nav>
         <main className={styles.eventDescription}>Event Description</main>
@@ -15,4 +46,4 @@ function AllEvents() {
   )
 }
 
-export default AllEvents
+export default AllEvents;
